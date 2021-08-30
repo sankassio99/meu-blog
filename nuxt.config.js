5cf3e7cb9ff6f53387e11380924fc5dc54321cef
Kássio <55934898+sankassio99@users.noreply.github.com>
2021-08-30 17:51:36 -0300

    Update nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,10 @@
 export default {
   // Target: https://go.nuxtjs.dev/config-target
   target: 'static',
-
+  ssr: false,
+  generate: {
+    fallback: true 
+  },
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     title: 'meu-blog',
