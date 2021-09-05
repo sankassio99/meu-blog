87a30439fb1a549987f3f2f87245a5e4b85cff66
sankassio99 <kciosantana@gmail.com>
2021-09-05 00:43:24 -0300

    new chores

---
+++ components/TextEditor.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="my-5">
+        <input placeholder="Titulo" type="text" v-model="titulo"
+                    class="text-1xl	 p-3 border border-gray-300 
+                    shadow-md outline-none w-full"> 
+        <vue-editor class="my-2 rounded border border-gray-200" v-model="content" />
+        <button type="button" v-on:click="salvarPost" 
+                    class="bg-blue-500 font-bold py-2 px-4 rounded text-white shadow-md">Salvar</button>
+    </div>
+</template>
+
+<script>
+import { VueEditor, Quill } from "vue2-editor";
+export default {
+    components: { VueEditor },
+
+    data: () => ({
+        content: "",
+        titulo: "",
+    }),
+    methods: {
+        salvarPost(){
+            this.$axios
+                .post("/api/posts",{
+                    titulo: this.titulo,
+                    conteudo: this.content
+                })
+                .then((response) => {
+                  alert("Salvo com sucesso")
+                })
+        },
+        
+    }
+}
+</script>
+
+<style>
+
+</style>
